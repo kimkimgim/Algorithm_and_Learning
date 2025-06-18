78ce856564b7934182456ce8b23b1417879fd94d
Seorin Kim <seorin66@naver.com>
2025-06-18 23:39:07 +0900

    [Silver III] Title: N과 M (1), Time: 140 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,15 +1,20 @@
-def dfs():
-    # 종료조건
-    if len(s) == M:
-        print(*s)       # print(' '.join(map(str,s)))
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+result = []
+
+def backtracking():
+
+    if len(result) == m:
+        print(' '.join(map(str,result)))
         return
 
-    for i in range(1, N+1):
-        if i not in s:
-            s.append(i)
-            dfs()
-            s.pop()
+    for i in range(1, n+1):
+        if i not in result:
+            result.append(i)
+            backtracking()
+            result.pop()
 
-N, M = map(int,input().split()) # 1부터 N 까지 / 고른 갯수(길이) : M
-s = []
-dfs()
+backtracking()
+
+
