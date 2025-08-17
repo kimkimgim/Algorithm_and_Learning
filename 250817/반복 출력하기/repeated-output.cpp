1e400800a3a38696a9dda6f80fc8e98473e313f6
Seorin Kim <seorin66@naver.com>
2025-08-17 23:20:02 +0900

    Title: 반복 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250817/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+void printN(int n)
+{   
+    string = "12345^&*()_"
+    for(int i = n; i<n; ++i)
+    {
+        cout << string << endl;
+    }
+}
+int main() {
+    int N;
+    cin >> N;
+    printN(N)
+    
+    return 0;
+}
