f3e6526375f0bfd68f3fff435ca51e1148a8c012
Seorin Kim <seorin66@naver.com>
2025-08-17 23:21:31 +0900

    Title: 반복 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250817/반복 출력하기/repeated-output.cpp
+++ 250817/반복 출력하기/repeated-output.cpp
@@ -7,16 +7,17 @@ int N;
 
 void printN(int n)
 {   
-    string = "12345^&*()_"
-    for(int i = n; i<n; ++i)
+    string a = "12345^&*()_";
+    for(int i = 0; i<n; ++i)
     {
-        cout << string << endl;
+        cout << a << endl;
     }
 }
 int main() {
     int N;
     cin >> N;
-    printN(N)
+
+    printN(N);
     
     return 0;
 }
