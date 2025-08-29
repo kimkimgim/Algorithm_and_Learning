ab65aa8f66ed4edf0aa61732821b2b716c920863
seorin66 <seorin66@naver.com>
2025-08-29 23:11:43 +0900

    C++ study

---
+++ C++/C++ 이론(K-mooc)/14/14분할구현.cpp
@@ -0,0 +1,12 @@
+#include "func.h"
+
+int main()
+{
+  int data = Add(10, 20);   // 호출
+
+  global = 20;
+  return 0;
+}
+
+
+
---
+++ C++/C++ 이론(K-mooc)/14/func.cpp
@@ -0,0 +1,13 @@
+#include "func.h"
+
+
+int Add(int a, int b)
+{
+  global = 10;
+  return a + b;
+}
+
+int Sub(int a, int b) // 여러 함수를 만들어 놓을 수 있음
+{
+  return a - b;
+}
