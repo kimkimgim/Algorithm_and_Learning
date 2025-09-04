465654fa08b54309f0f072e63402bb872ff5475b
Seorin Kim <seorin66@naver.com>
2025-09-04 20:46:06 +0900

    [Gold IV] Title: 연구소, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,17 +1,16 @@
 #include <iostream>
-#include <algorithm>
+#include <vector>
 #include <cstring>
+
 using namespace std;
 
-int n, m, ret;
-int arr[10][10];
-int visited[10][10];
+int n,m, ret;
+int arr[10][10], visited[10][10];
+vector<pair<int, int>> vir, wallP;
 
-vector<pair<int, int>> wallPossible, virus;
-
-const int dx[4] = {-1, 0, 1, 0};
-const int dy[4] = {0, 1, 0, -1};
-// 바이러스 퍼트리기
+// 방향
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
 
 void dfs(int x, int y)
 {
@@ -19,7 +18,7 @@ void dfs(int x, int y)
   {
     int nx = x + dx[i];
     int ny = y + dy[i];
-    if(nx < 0|| ny < 0 || nx >= n || ny >= m) continue;
+    if(nx <0 || ny < 0 || nx >= n || ny >= m) continue;
     if(visited[nx][ny]) continue;
     if(arr[nx][ny] == 1) continue;
     visited[nx][ny] = 1;
@@ -30,17 +29,19 @@ void dfs(int x, int y)
 int go()
 {
   memset(visited, 0, sizeof(visited));
-  for(pair<int, int> v : virus)
+  //바이러스 
+  for(pair<int, int> v : vir)
   {
     visited[v.first][v.second] = 1;
+    // 탐색
     dfs(v.first, v.second);
   }
 
-  // 0 을 세기
+  // 바이러스 퍼트리는게 완료되면 0 값 구하기
   int cnt = 0;
-  for(int i=0; i< n; ++i)
+  for(int i=0; i<n; ++i)
   {
-    for(int j=0; j <m; ++j)
+    for(int j=0; j<m; ++j)
     {
       if(arr[i][j] == 0 && visited[i][j] == 0) ++cnt;
     }
@@ -48,50 +49,48 @@ int go()
 
   return cnt;
 }
-
 int main()
 {
-  // 벽을 3개 세운다
-  // 바이러스를 퍼트린다(2 양옆에 0이 있으면 퍼트림)
-  // 남은 0의 갯수를 센다.
-  // 0의 갯수중 최댓값을 정답
   cin >> n >> m;
+
+  // 3개의 벽을 세우고
+  // 바이러스를 퍼트려야함
+  // 바이러스가 퍼지지 않은 나머지를 구함
+  // 최대갓 계산
 
   for(int i=0; i<n; ++i)
   {
     for(int j=0; j<m; ++j)
     {
       cin >> arr[i][j];
-      if(arr[i][j] == 0) wallPossible.push_back({i,j});
-      if(arr[i][j] == 2) virus.push_back({i,j});
+      if(arr[i][j] == 0) wallP.push_back({i,j});
+      if(arr[i][j] == 2) vir.push_back({i,j});
     }
   }
 
-
-  // 벽을 3개 세우기
-  for(int i=0; i<wallPossible.size(); ++i)
+  // 벽 세우기
+  for(int i=0; i<wallP.size(); ++i)
   {
-    for(int j=0; j < i; ++j)
+    for(int j=0; j<i; ++j)
     {
       for(int k=0; k<j; ++k)
       {
-        arr[wallPossible[i].first][wallPossible[i].second] = 1;
-        arr[wallPossible[j].first][wallPossible[j].second] = 1;
-        arr[wallPossible[k].first][wallPossible[k].second] = 1;
+        arr[wallP[i].first][wallP[i].second] = 1;
+        arr[wallP[j].first][wallP[j].second] = 1;
+        arr[wallP[k].first][wallP[k].second] = 1;
 
-        // 안정영역 수 반환
+        // 바이러스 퍼트는 함수를 통해 안전지대 값을 반환
         ret = max(ret, go());
 
-        arr[wallPossible[i].first][wallPossible[i].second] = 0;
-        arr[wallPossible[j].first][wallPossible[j].second] = 0;
-        arr[wallPossible[k].first][wallPossible[k].second] = 0;
-
+        arr[wallP[i].first][wallP[i].second] = 0;
+        arr[wallP[j].first][wallP[j].second] = 0;
+        arr[wallP[k].first][wallP[k].second] = 0;
       }
     }
   }
 
+  // 최종값
   cout << ret << '\n';
-
 
   return 0;
 }
