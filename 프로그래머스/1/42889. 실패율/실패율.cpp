f50d0bbff4b868a5a51ce778f5fde94bd4680467
Seorin Kim <seorin66@naver.com>
2025-09-07 17:48:13 +0900

    [level 1] Title: 실패율, Time: 9.61 ms, Memory: 9.88 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.cpp
+++ 프로그래머스/1/42889. 실패율/실패율.cpp
@@ -11,48 +11,46 @@ bool compare(pair<int, float>& a, pair<int, float>& b)
     }
     return a.second > b.second;
 }
-
 vector<int> solution(int N, vector<int> stages)
 {
     vector<int> answer;
-    // 스테이지에 도전한 플레이어 수
-    vector<float> c(N+2, 0.0);
-    // 스테이지 실패한 사람   
-    vector<float> f(N+2, 0.0);
     
-    // 스테이지에 도달한 수와, 실패한 수를 구하는 식
-    for(int i=0; i < stages.size(); ++i)
+    // 스테이지 도전한 사람
+    vector<float> c(N+2, 0.0);
+    // 스테이지에 실패한 사람
+    vector<float> f(N+2, 0.0);
+    for(int i=0; i<stages.size(); ++i)
     {
-        for(int j=1; j < stages[i]+1; ++j)
+        for(int j=1; j<=stages[i]; ++j)
         {
             ++c[j];
         }
         ++f[stages[i]];
     }
-    
+
     // 실패율 계산
-    vector<pair<int, float>> failRatio(N);
-    
+    vector<pair<int, float>> fR(N);
     for(int i=0; i<N; ++i)
     {
-        failRatio[i].first = i+1;
-        
+        fR[i].first = i+1;
         if(c[i+1] == 0)
         {
-            failRatio[i].second = 0;
+            fR[i].second = 0;
         }
         else
-        failRatio[i].second = f[i+1]/c[i+1];
+        {
+            fR[i].second = f[i+1]/c[i+1];
+        }
     }
     
-    // 정렬 
-    sort(failRatio.begin(), failRatio.end(), compare);
-    
+    // 정렬
+    sort(fR.begin(), fR.end(), compare);
+        
     // 답
     for(int i=0; i<N; ++i)
     {
-        answer.push_back(failRatio[i].first);
+        answer.push_back(fR[i].first);
     }
-    
+
     return answer;
 }
