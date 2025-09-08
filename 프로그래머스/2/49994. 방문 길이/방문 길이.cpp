2c2ed4b4e0cda0df7e74446c21744cde52b6889d
Seorin Kim <seorin66@naver.com>
2025-09-08 22:09:54 +0900

    [level 2] Title: 방문 길이, Time: 0.02 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
@@ -1,63 +1,47 @@
+#include <vector>
 #include <string>
 using namespace std;
 
-// 방문체크 : 시작지점, 도착지점, 방향
-// 아래처럼 선언했기에 -1로 넘어가면 인덱스 에러
 bool visited[11][11][4];
-
-// 방향  : 상 우 하 좌
+// 방향    // 상 우 하 좌
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
 
-// 방향 : dir 
-int todir(char dir)
+void move(int& x, int& y, int dir, int& cnt)
 {
-    int ret;
-    if(dir == 'U') ret = 0;
-    else if(dir == 'D') ret = 2;
-    else if(dir == 'R') ret = 1;
-    else if(dir == 'L') ret = 3;
+    int nx = x + dx[dir];
+    int ny = y + dy[dir];
     
-    return ret;
-}
-
-// 좌표평면 범위
-bool isNotValid(int& nx, int& ny)
-{
-    return nx < 0 || ny < 0 || nx > 10 || ny > 10 ;
-}
-
-// 반대 방향?
-int opposite_direction(int dir)
-{
-    return (dir+2) % 4;
-}
-
-int solution(string dirs) {
-    int answer = 0;   
-    // 시작 
-    // 위에서 [11][11] 을선언했기 때문에 마이너스로 넘어가면 에러
-    int x=5, y=5;
-    
-    for(auto c : dirs)
+    if(nx<0 || ny<0 || nx >10 || ny > 10) return;
+    if(visited[x][y][dir] == 0 && visited[nx][ny][(dir+2)%4] == 0)
     {
-        int dir = todir(c);
-        int nx = x + dx[dir];
-        int ny = y + dy[dir];
-        
-        if(isNotValid(nx, ny)) continue;
-        
-        if(visited[x][y][dir] == false)
-        {  
-            // (0,0)->(1,0) 과 (1,0) -> (0,0)은 같음
-            visited[x][y][dir] = true;
-            visited[nx][ny][opposite_direction(dir)] = true;
-            answer ++ ;
-        }
-        
-        x = nx;
-        y = ny;         
+        ++cnt;
+        visited[x][y][dir] = 1;
+        visited[nx][ny][(dir+2)%4] = 1; 
     }
+    x = nx;
+    y = ny;
+}
 
+int solution(string dirs)
+{
+    int answer = 0;
+    
+    // 시작지점 
+    int x = 5;
+    int y =5;
+    
+    // 처음 간 길만 센다
+    // visited 를 시작지점, 도착지점, 방향 그리고 그 반대 방향까지 체크 
+    
+    for(char c : dirs)
+    {
+        if(c == 'U') move(x, y, 0, answer);
+        else if(c == 'D') move(x, y, 2, answer);
+        else if(c == 'R') move(x, y, 1, answer);
+        else if(c == 'L') move(x, y, 3, answer);
+
+    }
+    
     return answer;
 }
