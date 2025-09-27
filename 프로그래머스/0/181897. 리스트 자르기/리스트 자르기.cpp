2657bad4efd3372879232065b9313413849afc5c
Seorin Kim <seorin66@naver.com>
2025-09-28 00:11:01 +0900

    [level 0] Title: 리스트 자르기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.cpp
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.cpp
@@ -10,24 +10,39 @@ vector<int> solution(int n, vector<int> slicer, vector<int> num_list) {
     int b = slicer[1];
     int c = slicer[2];
     
+    int st = 0;
+    int end = b + 1;
+    int step = 0;
+    
     if(n == 1)
     {
-       vector<int> sliced(num_list.begin(), num_list.begin()+b + 1);
-       answer = sliced;
+        st = 0;
+        end = b + 1;
     }
-    else if (n == 2)
+    else if( n == 2)
     {
-       vector<int> sliced(num_list.begin() + a, num_list.end());
-       answer = sliced;
+        st = a;
+        end = num_list.size();
     }
-    else if (n == 3)
+    else if( n == 3)
     {
-      vector<int> sliced (num_list.begin() + a, num_list.begin() + b + 1);
-      answer = sliced;
+        st = a;
+        end = b + 1;
     }
-    else if (n == 4)
+    else if( n == 4)
     {
-        for(int i=a; i <= b; i += c)
+        st = a;
+        end = b + 1;
+        step = c;   
+    }
+    
+    if(step == 0)
+    {
+        answer = vector<int>(num_list.begin() + st, num_list.begin() + end);
+    }
+    else 
+    {
+        for(int i = st; i < end; i += c)
         {
             answer.push_back(num_list[i]);
         }
