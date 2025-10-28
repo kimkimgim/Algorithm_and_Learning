f6bba6eac886c1c330944f93ec5b7473c81f4562
Seorin Kim <seorin66@naver.com>
2025-10-29 02:41:50 +0900

    [level 2] Title: 가장 큰 수, Time: 59.20 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -4,34 +4,28 @@
 
 using namespace std;
 
-bool compare(const string &a, string &b)
+bool compare(const string &a, const string &b)
 {
-    // temp = {"6", "2", "10"}
-    if (a + b > b + a)
+    if(a+b > b+a)
         return true;
     return false;
 }
-
 string solution(vector<int> numbers) {
     string answer = "";
+    
     vector<string> arr;
-    
-    // 가능한 모든 수를 만들기
-    // 모든 조합을 다 만든다면 시간 초과
-    for(int i=0; i<numbers.size(); ++i)
+    for(int n : numbers)
     {
-        arr.push_back(to_string(numbers.at(i)));
+        arr.push_back(to_string(n));
     }
-    
     sort(arr.begin(), arr.end(), compare);
     
-    for(int i=0; i< arr.size(); ++i)
+    for(string a : arr)
     {
-        answer += arr.at(i);
+        answer += a;
     }
     
-    if (answer[0] == '0')
-    return "0";
-
+    if(answer[0] == '0') answer = "0";
+    
     return answer;
 }
