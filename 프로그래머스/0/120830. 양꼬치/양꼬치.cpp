410fd983ce51d322ce959bfb710fed2a2511a7ad
Seorin Kim <seorin66@naver.com>
2025-10-28 23:35:38 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int k) {
+    int answer = 0;
+    
+    if(n/10 >= 1)
+    {
+        int i = n/10;
+        answer = n*12000 + (k-i)*2000;    
+    }
+    else
+    {
+        answer = n*12000 + k*2000;
+    }
+    
+    
+    return answer;
+}
