77d2aa9caa24c16d0bb5c291d7f97d4e178d879b
Seorin Kim <seorin66@naver.com>
2025-10-29 23:32:46 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -3,15 +3,28 @@
 
 using namespace std;
 
+
 long long solution(int n) {
     long long answer = 0;
-    int n2 = 2, n3 = 3;
+
+// 재귀는 시간초과 DP 로 풀어야함
+//     n = 1 1  => 1
+//     n = 2 11 / 2     => 2
+//     n = 3 111 / 21 / 12   => 3
+//     n = 4 1111 / 211/ 121 / 112 / 22 => 5
+//     n = 5 11111 / 2111 /1211 / 1121/ 1112/ 221/ 122/ 212 / => 8
     
-    if(n <= 3) return n;
-    for(int i = 4; i <= n; ++i) {
-        answer = (n2 + n3) % 1234567;
-        n2 = n3;
-        n3 = answer;
+    vector<long long> dp(n+1, 0);
+    if(n <= 3) return n % 1234567;
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for(int i=3; i<=n; ++i)
+    {
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
     }
+
+    answer = dp[n];
+    
     return answer;
 }
