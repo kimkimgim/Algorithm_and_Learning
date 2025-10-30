e2e6e5f251387919c8768274b97b1bc0790b13ae
Seorin Kim <seorin66@naver.com>
2025-10-31 03:18:14 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 1.45 ms, Memory: 4.48 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
@@ -1,64 +1,56 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
 int solution(vector<string> friends, vector<string> gifts) {
     int answer = 0;
-    
-    // 같으면 선물지수가 작은 사람이 큰사람에게
-    // 주고 받은 적이 없거나 같으면 
-    // 선물 지수 = 준 선물 - 받은 선물
-    // 선물 지수도 같다? 주고 받지 않음
-    
-    int size = friends.size();
-    // 각 친구들의 인덱스 지정
+    int k = friends.size();
     unordered_map<string, int> kakao;
     
-    // 서로 주고받은 선물
-    vector<vector<int>> graph(size, vector<int>(size+1, 0));
-    
-    // 다음달에 받을 선물
-    vector<int> next(size, 0);
-    
-    // 친구들 인덱스 지정
-    for(int i=0; i<size; ++i)
+    // 친구들 인덱스값 부여
+    for(int i=0; i<k; ++i)
     {
         kakao[friends[i]] = i;
     }
     
+    // 선물 저장 누가 얼마나 줬는지
+    vector<vector<int>> presents(k, vector<int>(k+1, 0));
     
-    for(string g : gifts)
+    for(string gi : gifts)
     {
-        auto pos = g.find(' ');
-        int to = kakao[g.substr(0, pos)];
-        int from = kakao[g.substr(pos+1)];
+        auto e = gi.find(" ");
+        int to = kakao[gi.substr(0, e)];
+        int from = kakao[gi.substr(e+1)];
         
-        // 준 사람 / 받은사람 그래프
-        ++graph[to][from];
-        // 선물지수
-        ++graph[to][size];
-        --graph[from][size];
+        ++presents[to][from];
+        ++presents[to][k];
+        --presents[from][k];
     }
     
-    for(int i=0; i<size; ++i)
+    // 받을 선물의 갯수
+    vector<int> next(k, 0);
+    
+    for(int i=0; i<k; ++i)
     {
-        for(int j=0; j<size; ++j)
+        for(int j=0; j<k; ++j)
         {
-            if(i == j) continue;
+            if(i==j) continue;
             
-            if(graph[i][j] > graph[j][i]) ++next[i];
-            else if(graph[i][j] == graph[j][i])
+            if(presents[i][j] > presents[j][i]) ++next[i];
+            else if(presents[i][j] == presents[j][i])
             {
-                if(graph[i][size] > graph[j][size]) ++next[i];
+                if(presents[i][k] > presents[j][k]) ++next[i];
             }
-            
         }
     }
     
+    answer = *max_element(next.begin(), next.end());
     
-    return answer = *max_element(next.begin(), next.end());
+    
+    
+    return answer;
 }
