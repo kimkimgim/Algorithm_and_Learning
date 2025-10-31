43a6e99a237cb25955da8d4266cfb89000f2ac7c
Seorin Kim <seorin66@naver.com>
2025-10-31 23:50:21 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -5,21 +5,15 @@ using namespace std;
 int solution(int n, int a, int b)
 {
     int answer = 0;
+    int round = 0;
     
-    int round = 1;
-    int groupsize = 2;
-    
-    // 그룹을 2명씩 묶어서 어디그룹에 있는지 
-    while(true)
+    while (a != b)
     {
-        // 같은 그룹인지 확인
-        // 0 1 => 0 , 1 2 => 1, 3 4 => 2
-        if((a-1)/groupsize == (b-1)/groupsize) break;
-        
-        groupsize *= 2;
-        round += 1;
+        a = (a+1) / 2;
+        b = (b+1) / 2;
+        ++round;
     }
-    
     answer = round;
+
     return answer;
 }
