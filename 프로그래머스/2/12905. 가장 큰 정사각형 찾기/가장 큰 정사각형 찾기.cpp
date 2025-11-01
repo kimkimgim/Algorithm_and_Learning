0072bff7cf99fa14cf9c06e18a0ced573780337a
Seorin Kim <seorin66@naver.com>
2025-11-01 23:52:50 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 4.36 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
@@ -1,25 +1,49 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
 int solution(vector<vector<int>> board)
 {
-    int answer = board[0][0];
-    int r = board.size();
-    int c = board[0].size();
+    int answer = 0;
     
-    for(int i=1;i<r;i++)
+    // DP
+    int n = board.size();
+    int m = board[0].size();
+    
+    for(int i=1; i<n; ++i)
     {
-        for(int j=1;j<c;j++)
+        for(int j=1; j<m; ++j)
         {
-            if(board[i][j]==1){
-                board[i][j] = 1 + min({board[i-1][j-1],board[i-1][j],board[i][j-1]});
-                answer = max(answer,board[i][j]);
+            if(board[i][j] == 1)
+            {
+                // 왼쪽, 왼쪽위, 위 확인
+                // min 3개 이상 {} 중괄호 필수
+                board[i][j] = min({board[i-1][j], board[i][j-1], board[i-1][j-1]}) +1;
+                answer = max(answer, board[i][j]);
+            }
+        }
+    }
+    
+        
+    // board 전체가 0 인경우
+    // 위쪽에서 행과 열의 각 첫번째 줄을 보지 않기 때문에 이 부분 처리해야함
+    if(answer == 0)
+    {
+        for(auto &row : board)
+        {
+            for(int c : row)
+            {
+                if(c == 1)
+                {
+                    answer = 1;
+                }
             }
         }
     }
 
-    return answer*answer;
+    
+    
+
+    return answer * answer;
 }
