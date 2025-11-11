7005cee4c1059a61e7bc4e8c6076491869a5e652
Seorin Kim <seorin66@naver.com>
2025-11-11 23:50:00 +0900

    [level 1] Title: 행렬의 덧셈, Time: 56.86 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer;
+    vector<int> arr;
+    for(int i=0; i<arr1.size(); ++i)
+    {
+        for(int j=0; j<arr1[i].size(); ++j)
+        {
+            int a = arr1[i][j] + arr2[i][j];
+            arr.push_back(a);
+        }
+        answer.push_back(arr);
+        arr.clear();
+    }
+    return answer;
+}
