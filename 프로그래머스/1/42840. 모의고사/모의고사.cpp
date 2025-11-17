ca0ab731cc38569c91b959716ca2c4bfc197f1f7
Seorin Kim <seorin66@naver.com>
2025-11-17 23:45:42 +0900

    [level 1] Title: 모의고사, Time: 0.13 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -1,42 +1,33 @@
+#include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 
-
-int howMany(vector<int> answers , vector<int> arr)
-{
-    int cnt = 0;
-    int as = arr.size();
-    for(int i=0; i<answers.size(); i++)
-    {
-        if(answers[i] == arr[(i)%as])
-        {
-            ++cnt;
-        }
-    }
-    return cnt;
-}
-vector<int> solution(vector<int> answers)
-{
+vector<int> solution(vector<int> answers) {
     vector<int> answer;
     
     vector<int> arr1 = {1, 2, 3, 4, 5};
     vector<int> arr2 = {2, 1, 2, 3, 2, 4, 2, 5};
     vector<int> arr3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5,};
     
-    int a = howMany(answers, arr1);
-    int b = howMany(answers, arr2);
-    int c = howMany(answers, arr3);
+    vector<int> arr(3);
     
-    vector<int> arr = {a, b, c};
-    int mx = max({a,b,c});
+
+    for(int i=0; i<answers.size(); ++i)
+    {
+        if(answers[i] == arr1[i%arr1.size()]) ++arr[0];
+        if(answers[i] == arr2[i%arr2.size()]) ++arr[1];
+        if(answers[i] == arr3[i%arr3.size()]) ++arr[2];
+
+    }
     
+    int maxScore = max({arr[0], arr[1], arr[2]});
     for(int i=0; i<arr.size(); ++i)
     {
-        if(arr[i] == mx) answer.push_back(i+1);
+        if(arr[i] == maxScore) answer.push_back(i+1);
     }
+
     
     return answer;
 }
