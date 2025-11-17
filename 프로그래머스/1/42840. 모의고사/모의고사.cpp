fc8eaa7ca078d755cf91041c2f6d2890e441a91b
Seorin Kim <seorin66@naver.com>
2025-11-17 23:45:52 +0900

    [level 1] Title: 모의고사, Time: 0.13 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -1,33 +1,9 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
-    
-    vector<int> arr1 = {1, 2, 3, 4, 5};
-    vector<int> arr2 = {2, 1, 2, 3, 2, 4, 2, 5};
-    vector<int> arr3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5,};
-    
-    vector<int> arr(3);
-    
-
-    for(int i=0; i<answers.size(); ++i)
-    {
-        if(answers[i] == arr1[i%arr1.size()]) ++arr[0];
-        if(answers[i] == arr2[i%arr2.size()]) ++arr[1];
-        if(answers[i] == arr3[i%arr3.size()]) ++arr[2];
-
-    }
-    
-    int maxScore = max({arr[0], arr[1], arr[2]});
-    for(int i=0; i<arr.size(); ++i)
-    {
-        if(arr[i] == maxScore) answer.push_back(i+1);
-    }
-
-    
     return answer;
 }
