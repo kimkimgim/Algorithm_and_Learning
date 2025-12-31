750fba0828aea82ba2880b61aea7919f6da2cc42
Seorin Kim <seorin66@naver.com>
2025-12-31 18:43:59 +0900

    [level 2] Title: 괄호 회전하기, Time: 9.14 ms, Memory: 3.7 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -1,52 +1,51 @@
 #include <string>
 #include <vector>
-#include <stack>
 #include <unordered_map>
+#include <stack>
 
 using namespace std;
 
-// map : 매번 자동정렬되기 때문에 시간이 더 들음
-// unordered_map : 자동정렬이 아니기 때문에 정렬할 필요가 없는 곳에서는 시간을 줄일 수 있음
-unordered_map<char, char> brackerPair = {{')','('}, {']','['}, {'}','{'}};
+unordered_map<char, char> bracket = {{')', '('}, {']', '['}, {'}', '{'}};
 
-// 문자열은 돌아가면서 변해야 함 그러니 원본을 건드려야함
-bool isValid(string& s, int start)
-{   
-    stack<char> stack;
-    unsigned int sz = s.size();
+bool isValid(string &s, int start)
+{
+    int sz = s.size();
+    stack<char> sk;
     
     for(int i=0; i<sz; ++i)
     {
-        // 시작지점이 하나씩 증가
-        // 차례대로 순회하면서 원형을 돌듯이 검사
-        char ch = s[(start+i)%sz];
-        
-        // count : key값이 있으면 1, 없으면 0
-        // ch의 값이 key에 있으면 true 없으면 false
-        if(brackerPair.count(ch))
+        char c = s[(start+i) % sz];
+        // bracket에 c 라는 키가 몇개 있냐?
+        if(bracket.count(c))
         {
-            if(stack.empty() || stack.top() != brackerPair[ch] ) return false;
-            stack.pop();
+            if(sk.empty() || sk.top() != bracket[c])
+            {
+                return false;
+            }
+            else
+            {
+                sk.pop();
+            }
         }
-        else
+        else 
         {
-            stack.push(ch);
+            sk.push(c);
         }
+            
     }
-    // 스택이 비어있으면 true;
-    return stack.empty();
+    return sk.empty();
 }
 
 
 int solution(string s) {
     
     int answer = 0;
+    int n = s.size();
     
-    // 괄호길이만큼 돌아야함
-    for(int i=0; i<s.size(); ++i)
+    for(int i=0; i<n; ++i)
     {
         answer += isValid(s, i);
     }
-    
+
     return answer;
 }
